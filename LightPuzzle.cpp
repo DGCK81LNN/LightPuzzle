6b535709bc2168b23d23e142a2faccb8aa6615b3
Five-Character Code <39875581+1stchrc@users.noreply.github.com>
2021-06-01 09:01:42 -0500

    Add files via upload

--- LightPuzzle.cpp
+++ LightPuzzle.cpp
@@ -1,4 +1,5 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <string.h>
 
 void initMatrix(int* mptr, int width, int height, int size, int mw){
@@ -36,9 +37,11 @@ int mult(int a, int b){
     return (a * b) % 2;
 }
 
-int trignize(int * ptr, int width, int height){
+int trignize(int * ptr, int width, int height, int * pZeros){
     int * temp = new int[width];
+    memset(pZeros, 0, sizeof(int) * height);
     int* pm = ptr;
+    int sc = 0;
     for(int i = 0; i < height; i++){
         int js = height - i - 1;
         int * jptr = ptr + width;
@@ -55,8 +58,9 @@ int trignize(int * ptr, int width, int height){
                 }
                 jjptr += width;
             }
-            delete [] temp;
-            return 1;
+            ptr+=width1;
+            pZeros[sc++] = i;
+            continue;
         }
         succ:
         for(int j = 0; j < js; j++){
@@ -72,6 +76,14 @@ int trignize(int * ptr, int width, int height){
         ptr += width1;
     }
     delete [] temp;
+    return sc;
+}
+
+int checkPara(int* ptr, int width, int height, int* pZeros, int sc){
+    for(int i = 0; i < sc; i++){
+        if(ptr[pZeros[i] * width + height])
+            return -1;
+    }
     return 0;
 }
 
@@ -85,21 +97,49 @@ void solve(int* ptr, int width, int height){
     }
 }
 
+int mSolve(int* ptr, int width, int height){
+    for(int i = height - 1; i >= 0; i--){
+        for(int j = height - 1; j > i; j--){
+            if(ptr[j + i * width] != 0){
+                for(int k = i; k < width; k++){
+                    ptr[i * width + k] = add(ptr[i * width + k], ptr[j * width + k]);
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+void mRead(int* ptr, int* pZeros, int sc, int idx, int mw, int size){
+    for(int i = 0; i < sc; i++){
+        int bit = (idx >> i) & 1;
+        ptr[pZeros[i] * mw + size] = bit;
+    }
+    for(int y = size - 1; y >= 0; y--){
+        int * p = ptr + y * mw;
+        for(int x = y + 1; x < size; x++){
+                *(p + size) = add(*(p + size),mult(p[x], ptr[x * mw + size]));
+        }
+    }
+}
+
 int main()
 {
     int width;
     int height;
     int * pMatrix;
-    scanf_s("%d", &width);
-    scanf_s("%d", &height);
+    int * pZeros;
+    scanf("%d", &width);
+    scanf("%d", &height);
     int size = width * height;
     int mw = size + 1;
     int matrixSize = mw * size;
     pMatrix = new int[matrixSize];
+    pZeros = new int[size];
     memset(pMatrix, 0, matrixSize * sizeof(int));
     initMatrix(pMatrix, width, height, size, mw);
     
-    int rs = trignize(pMatrix, mw, size);
+    int rs = trignize(pMatrix, mw, size, pZeros);
 
     if(rs == 0){
         solve(pMatrix, mw, size);
@@ -110,7 +150,21 @@ int main()
             printf("\n");
         }
     } else {
-        printf("Multiple or no solutions.");
+        mSolve(pMatrix, mw, size);
+        if(checkPara(pMatrix, mw, size, pZeros, rs)){
+            printf("No solution.\n");
+            return 0;
+        }
+        printf("%d solutions.\n",1 << rs);
+        int idx;
+        scanf("%d", &idx);
+        mRead(pMatrix, pZeros, rs, idx, mw, size);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                printf("%2d", pMatrix[(x + y * width) * mw + mw - 1]);
+            }
+            printf("\n");
+        }
     }
     return 0;
 }
